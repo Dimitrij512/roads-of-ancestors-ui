56e6d5fa527ef76255cfbd77cf79299b8569ccaa
Dimitrij512 <dmytro.andrusiv@gmail.com>
2019-02-08 22:18:18 +0200

    init commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { NouisliderModule } from 'ng2-nouislider';
+import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
+
+import { ComponentsComponent } from './components.component';
+import {GoalsComponent} from './goals/goals.component';
+import {ParticipationComponent} from './participation/participation.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        NgbModule,
+        NouisliderModule,
+        JwBootstrapSwitchNg2Module
+    ],
+    declarations: [
+        ComponentsComponent,
+        GoalsComponent,
+        ParticipationComponent
+    ],
+    exports:[ ComponentsComponent ]
+})
+export class ComponentsModule { }
