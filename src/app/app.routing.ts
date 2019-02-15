816e702595329e9017fb25c9a21882da636357cb
Slavik89 <terakrokos@gmail.com>
2019-02-15 15:25:00 +0200

    [Added] page with fund's regulations

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,10 +4,12 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { FundComponent } from './fund/fund.component';
 
-const routes: Routes =[
+const routes: Routes = [
     { path: 'home',             component: HomeComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'fund-regulations', component: FundComponent }
 ];
 
 @NgModule({
