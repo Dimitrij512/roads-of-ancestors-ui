7d0aac44c5145be17482af20766f2fd67cf9fd7c
Dimitrij512 <dmytro.andrusiv@gmail.com>
2019-02-16 23:48:07 +0200

    added odometer

--- src/app/shipyard/shipyard.component.ts
+++ src/app/shipyard/shipyard.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shipyard',
   templateUrl: './shipyard.component.html',
   styleUrls: ['./shipyard.component.scss']
 })
-export class ShipyardComponent implements OnInit {
+export class ShipyardComponent {
+    public number = 1239876;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
 }
