f5200fddc2adc7b79b042325c0e061e5e9931b75
dandrusiv <dmytro.andrusiv@gmail.com>
2020-12-07 18:19:09 +0200

    add the gallery for handmade events

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions} from "ngx-gallery";
 
 @Component({
   selector: 'app-events',
@@ -6,9 +7,11 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
-  @Input() text: string;
-  @Input() max = 200;
-  seeMore = false;
+    galleryOptions: NgxGalleryOptions[];
+    galleryImages: NgxGalleryImage[];
+    @Input() text: string;
+    @Input() max = 200;
+    seeMore = false;
 
   constructor() { }
 
@@ -26,6 +29,83 @@ export class EventsComponent implements OnInit {
         '            віку, хто, лише, має бажання. ' +
         'Захід для груп проводиться за попередньою домовленістю.\n' +
         '            Ми пропонуємо вам прийняти участь і навчитися наступним майстер-класам з прадавніх ремесел:';
+
+     this.galleryOptions = [
+            {
+                width: '600px',
+                height: '400px',
+                thumbnailsColumns: 4,
+                imageAnimation: NgxGalleryAnimation.Slide
+            },
+
+            {
+                breakpoint: 800,
+                width: '100%',
+                height: '600px',
+                imagePercent: 80,
+                thumbnailsPercent: 20,
+                thumbnailsMargin: 20,
+                thumbnailMargin: 20
+            },
+
+            {
+                breakpoint: 400,
+                preview: false
+            }
+        ];
+
+        this.galleryImages = [
+            {
+                small: './assets/img/projects/hand-made/1.jpg',
+                medium: './assets/img/projects/hand-made/1.jpg',
+                big: './assets/img/projects/hand-made/1.jpg'
+            },
+            {
+                small: './assets/img/projects/hand-made/2.jpg',
+                medium: './assets/img/projects/hand-made/2.jpg',
+                big: './assets/img/projects/hand-made/2.jpg'
+            },
+            {
+                small: './assets/img/projects/hand-made/3.jpg',
+                medium: './assets/img/projects/hand-made/3.jpg',
+                big: './assets/img/projects/hand-made/3.jpg'
+            },
+            {
+                small: './assets/img/projects/hand-made/4.jpg',
+                medium: './assets/img/projects/hand-made/4.jpg',
+                big: './assets/img/projects/hand-made/4.jpg'
+            },
+            {
+                small: './assets/img/projects/hand-made/5.jpg',
+                medium: './assets/img/projects/hand-made/5.jpg',
+                big: './assets/img/projects/hand-made/5.jpg'
+            },
+            {
+                small: './assets/img/projects/hand-made/6.jpg',
+                medium: './assets/img/projects/hand-made/6.jpg',
+                big: './assets/img/projects/hand-made/6.jpg'
+            },
+            {
+                small: './assets/img/projects/hand-made/7.png',
+                medium: './assets/img/projects/hand-made/7.png',
+                big: './assets/img/projects/hand-made/7.png'
+            },
+            {
+                small: './assets/img/projects/hand-made/8.jpg',
+                medium: './assets/img/projects/hand-made/8.jpg',
+                big: './assets/img/projects/hand-made/8.jpg'
+            },
+            {
+                small: './assets/img/projects/hand-made/9.jpg',
+                medium: './assets/img/projects/hand-made/9.jpg',
+                big: './assets/img/projects/hand-made/9.jpg'
+            },
+            {
+                small: './assets/img/projects/hand-made/10.jpg',
+                medium: './assets/img/projects/hand-made/10.jpg',
+                big: './assets/img/projects/hand-made/10.jpg'
+            }
+        ];
   }
 
 }
